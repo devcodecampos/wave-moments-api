970126ea814e8cbcbd1b8a075f790904cd1c8618
codecampos <devcodecampos@gmail.com>
2023-11-07 14:36:25 -0300

    feat: add express setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("typescript and eslint setup");
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+const port = process.env.SERVER_PORT;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Hello World" });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
