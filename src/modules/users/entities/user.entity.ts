f6e51bc10c3405d8014171df540d87a79cf9ce83
codecampos <devcodecampos@gmail.com>
2023-11-07 15:30:55 -0300

    feat: add user entity

---
+++ src/modules/users/entities/user.entity.ts
@@ -0,0 +1,41 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("users")
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ nullable: true })
+  bio: string;
+
+  @Column({ default: 0 })
+  followers_count: number;
+
+  @Column({ default: 0 })
+  following_count: number;
+
+  @Column({ select: false })
+  password_hash: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
+}
