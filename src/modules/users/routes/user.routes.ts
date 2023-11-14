bf5205ff42c330f52cc645b583f5292d09870ad8
codecampos <devcodecampos@gmail.com>
2023-11-14 10:34:19 -0300

    refactor: rename controller folder to controllers

--- src/modules/users/routes/user.routes.ts
+++ src/modules/users/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import UserController from "../controller/user.controller";
+import UserController from "../controllers/user.controller";
 import { validateUserCreationMiddleware } from "../middlewares/validate-user-creation.middleware";
 import { validateAuthTokenCreationMiddleware } from "../middlewares/validate-auth-token-creation.middleware";
 
